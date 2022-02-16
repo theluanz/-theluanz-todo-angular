d0adabd28a21e1da3e253fca193a552d0b9b639a
theluanz <luanzuffo.lz@gmail.com>
2022-02-16 14:43:53 -0300

    first component and removeTodo function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-angular';
+  public todos: Todo[] = [];
+  public title: String = 'My todos';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'caminar', false));
+    this.todos.push(new Todo(2, 'ir a escola', true));
+    this.todos.push(new Todo(3, 'programar', false));
+  }
+
+  removeTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+  markAsDone(todo: Todo) {}
+  markAsUndone(todo: Todo) {}
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,7 @@
+export class Todo {
+  constructor(
+    public id: Number,
+    public activity: String,
+    public done: boolean
+  ) {}
+}
